2cf541f9bc5bed01d3ab7ba1dd29cbde2044a182
av-hack10 <av10022007@gmail.com>
2026-05-28 10:17:23 +0530

    Add files via upload

---
+++ Day1-(1).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int n,i,sum=0;
+    printf("Enter the number of terms:");
+    scanf("%d",&n);
+    for( i=1; i<=n; i++)
+    {
+        sum = sum + i;
+    }
+    printf("sum = %d",sum);
+    return 0;
+}
